87ead76caeea1d49a7d708a2e4415ce3090f4221
gman <gman@codefreax.org>
2014-02-26 19:56:08 +0100

    Tree_expand: pack the TreeView widget appropriately
    Make the widget PACK_SHRINK if its model is empty, otherwise use
    PACK_EXPAND_WIDGET.

--- src/previews/tree_expand.cpp
+++ src/previews/tree_expand.cpp
@@ -4,7 +4,10 @@ Tree_expand::Tree_expand(Dir_preview* dp, Gtk::Box& box)
 	: Tree{dp, box}
 {
 	m_tree_view->set_sensitive(false);
-	box.pack_start(get_widget(), Gtk::PACK_EXPAND_WIDGET);
+
+	box.pack_start(get_widget(),
+			(m_tree_model->children().empty() ?
+			 Gtk::PACK_SHRINK : Gtk::PACK_EXPAND_WIDGET));
 
 	// Let's assume that the Tree_expand is the last Tree widget
 	// so we don't need a separator for it.
