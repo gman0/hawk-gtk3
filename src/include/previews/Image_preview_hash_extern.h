65363f3b902984ee1458720dac4718c50933c477
gman <gman@codefreax.org>
2014-04-16 20:23:19 +0200

    added license preamble + some refactoring

--- src/include/previews/Image_preview_hash_extern.h
+++ src/include/previews/Image_preview_hash_extern.h
@@ -1,3 +1,22 @@
+/*
+	Copyright (C) 2013-2014 Róbert "gman" Vašek <gman@codefreax.org>
+
+	This file is part of hawk-gtk3.
+
+	hawk-gtk3 is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+
+	hawk-gtk3 is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with hawk-gtk3.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef IMAGE_PREVIEW_HASH_EXTERN_H
 #define IMAGE_PREVIEW_HASH_EXTERN_H
 
--- src/previews/Dir_preview.cpp
+++ src/previews/Dir_preview.cpp
@@ -1,4 +1,22 @@
-#include <iostream>
+/*
+	Copyright (C) 2013-2014 Róbert "gman" Vašek <gman@codefreax.org>
+
+	This file is part of hawk-gtk3.
+
+	hawk-gtk3 is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+
+	hawk-gtk3 is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with hawk-gtk3.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "Window.h"
 #include "previews/Dir_preview.h"
 #include "previews/Tree_active.h"
